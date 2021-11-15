a0c1caa64aa727d2274ebeafe10e7ee104319a3c
Martin Weiss <xweiss3@mendelu.cz>
2021-11-15 18:16:06 +0100

    Update World header

--- ZOOProj/World.h
+++ ZOOProj/World.h
@@ -3,8 +3,21 @@
 #define ZOOPROJ_WORLD_H
 
 
-class World {
+#include <string>
+#include "Map.h"
 
+class World {
+private:
+    std::string m_name;
+    Map* m_map;
+    float calculateAttackDamage();
+    void invokeAttack(float damage);
+
+public:
+    World(std::string name, int size, int coordX, int coordY);
+    std::string getName();
+    Map* getMap();
+    void tryToInvokeAttack();
 };
 
 
