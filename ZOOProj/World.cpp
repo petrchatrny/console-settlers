99a2e5aa99c29cfe2c9cf758a5a5b4f04a25b101
Martin Weiss <xweiss3@mendelu.cz>
2021-11-15 18:17:26 +0100

    Added World class getters

--- ZOOProj/World.cpp
+++ ZOOProj/World.cpp
@@ -1,2 +1,15 @@
 
 #include "World.h"
+
+World::World(std::string name, int size, int coordX, int coordY) {
+    //TODO
+    this->m_name = name;
+}
+
+std::string World::getName() {
+    return this->m_name;
+}
+
+Map *World::getMap() {
+    return this->m_map;
+}
