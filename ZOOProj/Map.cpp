372c3095372566d1ac5095eaf3c87f3a03bb0c98
Martin Weiss <xweiss3@mendelu.cz>
2021-11-15 18:13:48 +0100

    Added getters

--- ZOOProj/Map.cpp
+++ ZOOProj/Map.cpp
@@ -1,2 +1,15 @@
 
 #include "Map.h"
+
+Map::Map(int size) {
+    //TODO
+    this->m_size = size;
+}
+
+int Map::getSize() {
+    return this->m_size;
+}
+
+std::vector<std::vector<Entity *>> Map::getRows() {
+    return this->m_rows;
+}
